92cb521fe454e644ba59f58f8543a1b01d2e7102
svistoplyasovya <svistoplyasovya@rambler.ru>
2017-02-16 11:45:53 +0300

    Добавили execute в класс Db

--- App/Db.php
+++ App/Db.php
@@ -5,9 +5,18 @@ namespace App;
 
 class Db
 {
+    protected $dbh;
+
     public function __construct()
     {
-        echo 'Hello Db';
+        $this->dbh = new \PDO('mysql:host=127.0.0.1;dbname=test','root','');
+    }
+
+    public function execute($sql)
+    {
+        $sth = $this->dbh->prepare($sql);
+        $res = $sth->execute();
+        return $res;
     }
 
 
--- index.php
+++ index.php
@@ -3,3 +3,5 @@
 require __DIR__ . '/autoload.php';
 
 $db = new \App\Db();
+$res = $db->execute('CREATE TABLE foo (id SERIAL)');
+var_dump($res);
