a309008c4b9baf9bbed08a1327f5e86ef3074335
Dmitriy <Svistoplyasovya@rambler.ru>
2017-04-11 13:08:16 +0300

    Bootstrap

--- index.php
+++ index.php
@@ -1,10 +1,3 @@
 <?php
 
-require __DIR__ . '/autoload.php';
-
-$user = new \App\Models\User();
-$user->name = 'Vasya';
-$user->email = 'v@pupkin.ru';
-$user->insert();
-
-//var_dump($users);
+include __DIR__ . '/App/templates/index.php';
